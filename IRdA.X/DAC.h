3a49579dd7533394e7e52104cc08599842595e87
10158431 <10158431@FLSB67XHR2.flowserve.net>
2020-06-01 21:22:00 -0500

    seems to be receiving correct commands with timer0 in 16bit mode and Fosc = 1MHz. added code to set TMR0H = 0x00 at init tried adding code to turn receiver on and off doesn't seem to work. Tried to get it working with external 4MHz oscillator too many problems. Going with internal 1MHz. Initial commit switching to LF18313. Got 1 second tic working with sleep and NEC messages still received. Added RTC filtes. Tested delay with 10s, works. intital commit to add DAC files to control LED brightness

---
+++ IRdA.X/DAC.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   DAC.h
+ * Author: 10158431
+ *
+ * Created on June 1, 2020, 9:21 PM
+ */
+
+#ifndef DAC_H
+#define	DAC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DAC_H */
+
--- IRdA.X/nbproject/Makefile-default.mk
+++ IRdA.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c Que.c Retarget.c uart_UCA0.c Interrupts.c NEC.c PowerManagement.c tmr_TMR1.c LED.c Oscillator.c ccp_CCP1.c tmr_TMR0.c RTC.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c Que.c Retarget.c uart_UCA0.c Interrupts.c NEC.c PowerManagement.c tmr_TMR1.c LED.c Oscillator.c ccp_CCP1.c tmr_TMR0.c RTC.c DAC.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/Que.p1 ${OBJECTDIR}/Retarget.p1 ${OBJECTDIR}/uart_UCA0.p1 ${OBJECTDIR}/Interrupts.p1 ${OBJECTDIR}/NEC.p1 ${OBJECTDIR}/PowerManagement.p1 ${OBJECTDIR}/tmr_TMR1.p1 ${OBJECTDIR}/LED.p1 ${OBJECTDIR}/Oscillator.p1 ${OBJECTDIR}/ccp_CCP1.p1 ${OBJECTDIR}/tmr_TMR0.p1 ${OBJECTDIR}/RTC.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/Que.p1.d ${OBJECTDIR}/Retarget.p1.d ${OBJECTDIR}/uart_UCA0.p1.d ${OBJECTDIR}/Interrupts.p1.d ${OBJECTDIR}/NEC.p1.d ${OBJECTDIR}/PowerManagement.p1.d ${OBJECTDIR}/tmr_TMR1.p1.d ${OBJECTDIR}/LED.p1.d ${OBJECTDIR}/Oscillator.p1.d ${OBJECTDIR}/ccp_CCP1.p1.d ${OBJECTDIR}/tmr_TMR0.p1.d ${OBJECTDIR}/RTC.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/Que.p1 ${OBJECTDIR}/Retarget.p1 ${OBJECTDIR}/uart_UCA0.p1 ${OBJECTDIR}/Interrupts.p1 ${OBJECTDIR}/NEC.p1 ${OBJECTDIR}/PowerManagement.p1 ${OBJECTDIR}/tmr_TMR1.p1 ${OBJECTDIR}/LED.p1 ${OBJECTDIR}/Oscillator.p1 ${OBJECTDIR}/ccp_CCP1.p1 ${OBJECTDIR}/tmr_TMR0.p1 ${OBJECTDIR}/RTC.p1 ${OBJECTDIR}/DAC.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/Que.p1.d ${OBJECTDIR}/Retarget.p1.d ${OBJECTDIR}/uart_UCA0.p1.d ${OBJECTDIR}/Interrupts.p1.d ${OBJECTDIR}/NEC.p1.d ${OBJECTDIR}/PowerManagement.p1.d ${OBJECTDIR}/tmr_TMR1.p1.d ${OBJECTDIR}/LED.p1.d ${OBJECTDIR}/Oscillator.p1.d ${OBJECTDIR}/ccp_CCP1.p1.d ${OBJECTDIR}/tmr_TMR0.p1.d ${OBJECTDIR}/RTC.p1.d ${OBJECTDIR}/DAC.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/Que.p1 ${OBJECTDIR}/Retarget.p1 ${OBJECTDIR}/uart_UCA0.p1 ${OBJECTDIR}/Interrupts.p1 ${OBJECTDIR}/NEC.p1 ${OBJECTDIR}/PowerManagement.p1 ${OBJECTDIR}/tmr_TMR1.p1 ${OBJECTDIR}/LED.p1 ${OBJECTDIR}/Oscillator.p1 ${OBJECTDIR}/ccp_CCP1.p1 ${OBJECTDIR}/tmr_TMR0.p1 ${OBJECTDIR}/RTC.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/Que.p1 ${OBJECTDIR}/Retarget.p1 ${OBJECTDIR}/uart_UCA0.p1 ${OBJECTDIR}/Interrupts.p1 ${OBJECTDIR}/NEC.p1 ${OBJECTDIR}/PowerManagement.p1 ${OBJECTDIR}/tmr_TMR1.p1 ${OBJECTDIR}/LED.p1 ${OBJECTDIR}/Oscillator.p1 ${OBJECTDIR}/ccp_CCP1.p1 ${OBJECTDIR}/tmr_TMR0.p1 ${OBJECTDIR}/RTC.p1 ${OBJECTDIR}/DAC.p1
 
 # Source Files
-SOURCEFILES=main.c Que.c Retarget.c uart_UCA0.c Interrupts.c NEC.c PowerManagement.c tmr_TMR1.c LED.c Oscillator.c ccp_CCP1.c tmr_TMR0.c RTC.c
+SOURCEFILES=main.c Que.c Retarget.c uart_UCA0.c Interrupts.c NEC.c PowerManagement.c tmr_TMR1.c LED.c Oscillator.c ccp_CCP1.c tmr_TMR0.c RTC.c DAC.c
 
 
 
@@ -198,6 +198,14 @@ ${OBJECTDIR}/RTC.p1: RTC.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/RTC.d ${OBJECTDIR}/RTC.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/RTC.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/DAC.p1: DAC.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/DAC.p1.d 
+	@${RM} ${OBJECTDIR}/DAC.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/DAC.p1 DAC.c 
+	@-${MV} ${OBJECTDIR}/DAC.d ${OBJECTDIR}/DAC.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/DAC.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -302,6 +310,14 @@ ${OBJECTDIR}/RTC.p1: RTC.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/RTC.p1 RTC.c 
 	@-${MV} ${OBJECTDIR}/RTC.d ${OBJECTDIR}/RTC.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/RTC.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/DAC.p1: DAC.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/DAC.p1.d 
+	@${RM} ${OBJECTDIR}/DAC.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/DAC.p1 DAC.c 
+	@-${MV} ${OBJECTDIR}/DAC.d ${OBJECTDIR}/DAC.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/DAC.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
